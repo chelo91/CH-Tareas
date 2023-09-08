25d8283d33e3189cc62ac7955f1ab472dc7730c1
Marcelo <marzelo.aguirre@gmail.com>
2023-09-08 11:22:53 -0500

    fixes post entrega

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
-import ProductManager from './models/ProductManager.js';
+//import ProductManager from './models/ProductManager.js';
+
+class ProductManager {
+    constructor() {
+        ProductManager.propProduct = [
+            'title',
+            'description',
+            'price',
+            'thumbnail',
+            'code',
+            'stock'
+        ];
+        this.arrayProduct = [];
+        this.nextProductId = 1;
+    }
+    getProducts() {
+        return this.arrayProduct;
+    }
+    addProduct(newProduct) {
+        if (!this.checkProductProp(product)) {
+            console.error("Falta propiedades");
+            return null;
+
+        }
+        if (!(this.getProductByCode(newProduct.code) == null)) {
+            console.error("Codigo repetido");
+            return null;
+        }
+        newProduct.id = this.nextProductId;
+        this.arrayProduct.push(newProduct);
+        this.nextProductId++;
+        return newProduct.id;
+    }
+    checkProductProp(product) {
+        return ProductManager.propProduct.every((prop) => product.hasOwnProperty(prop));
+    }
+    getProductById(id) {
+        return this.arrayProduct.find(product => product.id === id) || (console.log('Not found'), null);
+    }
+    getProductByCode(code) {
+        return this.arrayProduct.find(product => product.code === code) || (console.log('Not found'), null);
+    }
+}
 
 console.log("\nSe creará una instancia de la clase “ProductManager");
 const pm = new ProductManager();
--- models/ProductManager.js
+++ models/ProductManager.js
@@ -1,20 +1,33 @@
 export default class ProductManager {
     constructor() {
         this.arrayProduct = [];
+        ProductManager.propProduct = [
+            'title',
+            'description',
+            'price',
+            'thumbnail',
+            'code',
+            'stock'
+        ];
         this.nextProductId = 1;
     }
     getProducts() {
-        return this.arrayProduct ;
+        return this.arrayProduct;
     }
     addProduct(newProduct) {
-        if (this.getProductByCode(newProduct.code) == null) {
-            newProduct.id = this.nextProductId;
-            this.arrayProduct.push(newProduct);
-            this.nextProductId++;
-            return newProduct.id;
+        if (checkProductProp(product)) {
+            if (this.getProductByCode(newProduct.code) == null) {
+                newProduct.id = this.nextProductId;
+                this.arrayProduct.push(newProduct);
+                this.nextProductId++;
+                return newProduct.id;
+            }
         }
         console.error("Not found");
         return null;
+    }
+    checkProductProp(product) {
+        ProductManager.propProduct.every((prop) => product.hasOwnProperty(prop));
     }
     getProductById(id) {
         return this.arrayProduct.find(product => product.id === id) || null;
