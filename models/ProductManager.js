e76b3afa67b63fb6f4f71a89921b89513775c45b
Marcelo <marzelo.aguirre@gmail.com>
2023-09-13 08:53:19 -0500

    comienzo de la tarea 2

--- models/ProductManager.js
+++ models/ProductManager.js
@@ -1,6 +1,5 @@
 export default class ProductManager {
-    constructor() {
-        this.arrayProduct = [];
+    constructor(path) {
         ProductManager.propProduct = [
             'title',
             'description',
@@ -9,30 +8,45 @@ export default class ProductManager {
             'code',
             'stock'
         ];
+        this.arrayProduct = [];
         this.nextProductId = 1;
+        this.path = path;
     }
     getProducts() {
         return this.arrayProduct;
     }
     addProduct(newProduct) {
-        if (checkProductProp(product)) {
-            if (this.getProductByCode(newProduct.code) == null) {
-                newProduct.id = this.nextProductId;
-                this.arrayProduct.push(newProduct);
-                this.nextProductId++;
-                return newProduct.id;
-            }
+        if (!this.checkProductProp(product)) {
+            console.error("Falta propiedades");
+            return null;
+
         }
-        console.error("Not found");
-        return null;
+        if (!(this.getProductByCode(newProduct.code) == null)) {
+            console.error("Codigo repetido");
+            return null;
+        }
+        newProduct.id = this.nextProductId;
+        this.arrayProduct.push(newProduct);
+        this.nextProductId++;
+        return newProduct.id;
     }
     checkProductProp(product) {
-        ProductManager.propProduct.every((prop) => product.hasOwnProperty(prop));
+        return ProductManager.propProduct.every((prop) => product.hasOwnProperty(prop));
     }
     getProductById(id) {
-        return this.arrayProduct.find(product => product.id === id) || null;
+        return this.arrayProduct.find(product => product.id === id) || (console.log('Not found'), null);
     }
     getProductByCode(code) {
-        return this.arrayProduct.find(product => product.code === code) || null;
+        return this.arrayProduct.find(product => product.code === code) || (console.log('Not found'), null);
+    }
+    updateProduct(id, product) {
+        return this.arrayProduct.find(product => product.id === id) || (console.log('Not found'), null);
+    }
+    deleteProduct(id) {
+        const index = this.arrayProduct.findIndex(product => product.id === id);
+        if (index !== -1) {
+            this.arrayProduct.splice(index, 1);
+        }
+        return index !== -1;
     }
 }
